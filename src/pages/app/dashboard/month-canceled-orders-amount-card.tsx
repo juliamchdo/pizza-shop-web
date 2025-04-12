83a0298975f3a5bf6c0e124a4d4f121c976cec41
juliamchdo <juliamachado226@gmail.com>
2025-04-12 09:08:54 -0300

    add main home routes

---
+++ src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -0,0 +1,23 @@
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { DollarSign } from "lucide-react";
+
+export function MonthCanceledOrdersAmountCard() {
+  return (
+    <Card>
+      <CardHeader className="flex-row space-y-0 items-center justify-between pb-2">
+        <CardTitle className="text-base font-semibold">
+          Cancelamentos (mês)
+        </CardTitle>
+        <DollarSign className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+      <CardContent className="space-y-1">
+        <span className="text-2xl font-bold tracking-tight">20</span>
+
+        <p className="text-xs text-muted-foreground">
+          <span className="text-emerald-500 dark:text-emerald-400">-3%</span> em
+          relação ao mês apassado
+        </p>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,48 @@
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { ArrowRight, Search, X } from "lucide-react";
+import { OrderDetails } from "./order-details";
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        89in3h34131h493f
+      </TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Júlia de Lima Machado</TableCell>
+      <TableCell className="font-medium">R$140,00</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
+}
